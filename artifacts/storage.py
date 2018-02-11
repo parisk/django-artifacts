9a0ec71127fbee1f0c2f151f5636e5f67da3885d
Paris Kasidiaris <paris@sourcelair.com>
2018-02-11 21:14:20 +0200

    Start tying things up

--- artifacts/storage.py
+++ artifacts/storage.py
@@ -1,6 +1,26 @@
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+import delegator
+
+
+WEBPACK_DIRS = [
+    'web',
+]
 
 
 class ArtifactsStorage(ManifestStaticFilesStorage):
-    def post_process(self, *args, **kwargs):
-        pass
+    def post_process(self, paths, **options):
+        for path in WEBPACK_DIRS:
+            webpack_dir_path = self.path(path)
+            webpack_bin_path = f'{webpack_dir_path}/node_modules/webpack/bin/webpack.js'
+            print(f'Running webpack in "{webpack_dir_path}"')
+            w = delegator.run(
+                ['node', webpack_bin_path, '.'], cwd=webpack_dir_path,
+            )
+            import pdb
+            pdb.set_trace()
+            print(w.return_code)
+            print(w.out)
+            print(w.err)
+
+        # return super().post_process(paths, **options)
+        return []
